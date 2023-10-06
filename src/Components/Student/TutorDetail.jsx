5713c55e2122166cb70a7184fa3bab13e196bdd9
shijith pk <shijith.thalassery@gmail.com>
2023-10-06 19:09:52 +0530

    startBookingui

--- src/Components/Student/TutorDetail.jsx
+++ src/Components/Student/TutorDetail.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useEffect}from 'react';
+import React, { useState, useEffect } from 'react';
 import StudentNavbar from './navbarFooter/StudentNavbar';
 import { useParams } from 'react-router-dom';
 import { SERVER } from '../../Services/helper';
@@ -10,17 +10,17 @@ function TutorDetail() {
     const fetchTutors = async (id) => {
         console.log(id, 'id of the tutor');
         try {
-          const response = await axios.get(`${SERVER}tutorDetail/${id}`);
-          setTutorDetail(response.data);
+            const response = await axios.get(`${SERVER}tutorDetail/${id}`);
+            setTutorDetail(response.data.tutorDetail);
         } catch (error) {
-          console.error("Error fetching tutor detail:", error);
+            console.error("Error fetching tutor detail:", error);
         }
-      };
-      
+    };
+
     useEffect(() => {
-      fetchTutors(id);
+        fetchTutors(id);
     }, []);
-    console.log(tutorDetail,'this is tutorDetail')
+    console.log(tutorDetail, 'this is tutorDetail')
 
 
     return (
@@ -31,7 +31,7 @@ function TutorDetail() {
                     <div className='bg-blue-600 flex flex-col md:flex-row w-full h-2/3 items-center'>
                         <div className='mb-4 md:mb-0'>
                             <div className="w-48 h-48 border border-gray-300 rounded-md">
-                                <img src="your-image-url.jpg" alt="Your Image" className="w-full h-full object-cover shadow-md" />
+                                <img src={tutorDetail.profilePhoto} alt="Your Image" className="w-full h-full object-cover shadow-md" />
                             </div>
                         </div>
                         <div className='md:ml-4'>
@@ -41,6 +41,13 @@ function TutorDetail() {
                         </div>
                     </div>
                     <div className='bg-white mt-2 flex flex-col md:flex-row justify-center w-full'>
+                        <button
+                            type="button"
+                            className="mx-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                            onClick
+                        >
+                            Select Time
+                        </button>
                         <button
                             type="button"
                             className="mt-2 md:mt-0 mb-2 md:mb-0 mx-2 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
@@ -56,7 +63,7 @@ function TutorDetail() {
                             Book now
                         </button>
                     </div>
-                    <div className='bg-yellow-400 w-full mt-2 flex flex-col'>
+                    <div className='bg-yellow-400 w-full mt-2 flex flex-col justify-center'>
                         <h1>Review</h1>
                         <div className="h-1 w-full bg-blue-900"></div>
                         <div className=' justify-center text-center'>
