d2241329902b002258177d0c03a9c5d97b19a85d
shijith pk <shijith.thalassery@gmail.com>
2023-11-25 02:13:31 +0530

    urlchangetwo

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:4002"), []);
+  const socket = useMemo(() => io("www.speaksphere.shop"), []);
 
   return (
     <SocketContext.Provider value={socket}>
